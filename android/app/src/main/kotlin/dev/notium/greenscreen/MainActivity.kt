84af2d9a9b89425718458092daf0193791d9b1f6
Leon Recker <leon.recker@gmx.de>
2025-05-26 09:10:24 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/dev/notium/greenscreen/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.notium.greenscreen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'dart:convert';
+
+import 'package:app_usage/app_usage.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'WebView Data Bridge',
+      home: WebViewPage(),
+    );
+  }
+}
+
+class WebViewPage extends StatefulWidget {
+  const WebViewPage({super.key});
+
+  @override
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  late final WebViewController _controller;
+
+  void getUsageStats() async {
+    DateTime endDate = DateTime.now();
+    DateTime startDate = endDate.subtract(Duration(hours: 1));
+    List<AppUsageInfo> infoList =
+    await AppUsage().getAppUsage(startDate, endDate);
+    String json = await YourNativePlugin.getDeviceData();
+    print(json);
+
+    for (var info in infoList) {
+      print(info.toString());
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    getUsageStats();
+
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..addJavaScriptChannel(
+        'fetchNativeData',
+        onMessageReceived: (JavaScriptMessage message) async {
+          // Call your native plugin to get device data
+          final data = await YourNativePlugin.getDeviceData();
+          // Send the data back to the web content by invoking a JS callback
+          final jsonData = Uri.encodeComponent(data.toString());
+          _controller.runJavaScript(
+              "window.onNativeData && window.onNativeData(decodeURIComponent('$jsonData'));"
+          );
+        },
+      )
+      ..loadRequest(
+        Uri.parse('http://10.0.2.2:8080'),
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('WebView Data Bridge')),
+      body: WebViewWidget(controller: _controller),
+    );
+  }
+}
+
+class YourNativePlugin {
+  static Future<String> getDeviceData() async {
+    DateTime endDate = DateTime.now();
+    DateTime startDate = endDate.subtract(const Duration(hours: 500));
+
+    List<AppUsageInfo> infoList = await AppUsage().getAppUsage(startDate, endDate);
+
+    List<Map<String, dynamic>> jsonList = infoList.map((info) => {
+      'packageName': info.packageName,
+      'appName': info.appName,
+      'usage': info.usage.inSeconds,
+      'startTime': info.startDate.toIso8601String(),
+      'endTime': info.endDate.toIso8601String(),
+    }).toList();
+
+    return jsonEncode(jsonList);
+  }
+}
